afe3f50bbed2a73c70d965724e3c5dfcbb1fb86f
Marco <marco.fura@unmsm.edu.pe>
2022-11-16 11:18:28 -0500

    feat(searcher): add searcher filter

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets,permissions
+from rest_framework import viewsets,permissions,filters
 from movies import serializers
 from movies import models
 # Create your views here.
@@ -11,6 +11,8 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset=models.Movie.objects.all().order_by("-releaseYear")
     permissions_classes=[permissions.AllowAny]
     serializer_class=serializers.MovieSerializer
+    filter_backends=[filters.SearchFilter]
+    search_fields=["title"]
 
 class PosterArtViewSet(viewsets.ModelViewSet):
     queryset=models.PosterArt.objects.all()
