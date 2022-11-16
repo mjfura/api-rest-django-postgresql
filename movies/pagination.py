b7cf204c12d6832652cf8a9bcfebbbfbb8ca691e
Marco <marco.fura@unmsm.edu.pe>
2022-11-15 23:33:04 -0500

    refactor(serializers): add depth to serializers

--- movies/pagination.py
+++ movies/pagination.py
@@ -1,6 +1,9 @@
 from rest_framework import pagination
 from rest_framework.response import Response
-
+import environ
+env=environ.Env(
+    DEBUG=(bool, False)
+)
 class CustomPagination(pagination.PageNumberPagination):
     def get_paginated_response(self, data):
         print("pagination",self.__dict__)
@@ -8,7 +11,7 @@ class CustomPagination(pagination.PageNumberPagination):
         for x in range(self.page.paginator.num_pages):
             array.append({
                 "label":x+1,
-                "link":"http://localhost:8000/api/movies/?page={page}".format(page=x+1),
+                "link":"{host}/api/movies/?page={page}".format(page=x+1,host=env("HOST")),
                 "active":self.page.number==x+1
             })
         return Response({
